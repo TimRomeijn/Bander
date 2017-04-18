a38466ed74c0b7e82eea3345ba457441f678da84
TimRomeijn <tim28@live.nl>
2017-04-18 15:59:07 +0200

    basic layout added to profile
    basic layout added to profile

--- BanderApp/www/js/app.js
+++ BanderApp/www/js/app.js
@@ -1,7 +1,7 @@
 var functions = {
      
 
-    postRegisterData: function() {
-        $.post(API.endPoints.register, $('#registerForm').serialize())
+    getProfileData : function() {
+        
     }
 }
