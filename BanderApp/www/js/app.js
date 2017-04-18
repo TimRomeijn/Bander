4dc3eb425fbe5a2ec3c7e85e3b8f7eb18ec8a1e4
TimRomeijn <tim28@live.nl>
2017-04-18 16:52:07 +0200

    Start to adding profile data to dom list
    Start to adding profile data to dom list

--- BanderApp/www/js/app.js
+++ BanderApp/www/js/app.js
@@ -1,7 +1,12 @@
 var functions = {
      
 
-    getProfileData : function() {
-        
+    getProfileData : function(id) {
+        var apiRequest = API.apiRequest ("GET", API.endPoints.profile, {id: id});
+        return apiRequest;
     }
+}
+
+function listData(image, name, email, tel_number, genre_name, instrument_name) {
+    $(image).attr("src", API.endPoints.profile);
 }
